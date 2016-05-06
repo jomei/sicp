e9d8e48f22c42f0e5cd1175398f50f40cd847d9c
anatolynosov <jomeisama@gmail.com>
2016-05-06 16:47:44 +0300

    Tests

--- test/sicp/ch1/12_test.clj
+++ test/sicp/ch1/12_test.clj
@@ -8,4 +8,4 @@
 
 (deftest test-recur-fn
   (testing "Pascal triangle should return [1 4 6 4 1] for 5th line"
-    (is (= (pascal-triangle 1) [1 1]))))
+    (is (= (pascal-triangle 5) [1 4 6 4 1]))))
