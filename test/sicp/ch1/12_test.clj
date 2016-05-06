aa393b0bbefbff0af93e96395905279a79dcc6ad
anatolynosov <jomeisama@gmail.com>
2016-05-06 16:47:24 +0300

    Tests

--- test/sicp/ch1/12_test.clj
+++ test/sicp/ch1/12_test.clj
@@ -1,3 +1,11 @@
 (ns sicp.ch1.12-test
   (:require [clojure.test :refer :all]
             [sicp.ch1.12 :refer :all]))
+
+(deftest test-recur-fn
+  (testing "Pascal triangle should return [1 1] for second line"
+    (is (= (pascal-triangle 1) [1 1]))))
+
+(deftest test-recur-fn
+  (testing "Pascal triangle should return [1 4 6 4 1] for 5th line"
+    (is (= (pascal-triangle 1) [1 1]))))
