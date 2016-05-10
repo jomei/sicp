310b98ed388465bbc2cc1055e3767af7109d1567
anatolynosov <jomeisama@gmail.com>
2016-05-10 19:14:09 +0300

    Solution 12

--- src/sicp/ch1/12.clj
+++ src/sicp/ch1/12.clj
@@ -1,18 +1,26 @@
 (ns sicp.ch1.12)
 
-(defn get-line [prev-line]
-  (for [x prev-line] (println x))
-  )
+(defn get-line [prev-line result]
+  (if (empty? prev-line) (conj result 1))
+  (if (empty? result)
+    (do
+      (conj result 1)
+      (get-line prev-line result))
+    (do
+      (conj result (+ (first prev-line) (first(rest prev-line))))
+      (get-line (rest prev-line) result)))
+)
 
+(get-line [1 2 3] [])
+
+(+ nil 1)
 (defn pascal-iter [line n count]
   (if (= count n)
     line
     (get-line line)))
 
-(get-line [0 1 2 3 4])
-
-
-(defn pascal-triangle [n]
+(defn pascal-triangle
   "Returns nth line of pascal triangle"
+  [n]
   (pascal-iter [1] n 1)
   )
