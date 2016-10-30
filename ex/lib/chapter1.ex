a9c1d57c243b83b3af239ee254bd890e7a518a39
Anatoly Nosov <jomeisama@gmail.com>
2016-10-30 10:59:17 +0300

    Ex 8

--- ex/lib/chapter1.ex
+++ ex/lib/chapter1.ex
@@ -1,9 +1,39 @@
 defmodule Chapter1 do
-	def ex_3(a, b, c) do
-		[a, b, c]
-		|> Enum.sort
-		|> Enum.reverse
-		|> Enum.take(2)
-		|> (fn([x, y])-> x + y end).()
+	defmodule Ex3 do
+		def run(a, b, c) do
+			[a, b, c]
+			|> Enum.sort
+			|> Enum.reverse
+			|> Enum.take(2)
+			|> (fn([x, y])-> x + y end).()
+		end
 	end
+
+	defmodule Ex8 do
+		# sqrt
+		def run(x) do
+			Float.round(iter(1, x),2)
+		end
+		
+		defp iter(guess, x) do
+			if good?(guess, x) do
+				guess
+			else
+				iter(improve(guess, x), x)
+			end
+		end
+
+		defp improve(guess, x) do
+			new_guess = x / guess
+			avearge guess, new_guess
+		end
+
+		defp good?(guess, x) do
+			abs(guess * guess - x) < 0.01
+		end
+		defp avearge(x, y), do: (x + y) / 2
+	end
+
+
+	
 end
--- ex/test/chapter1_test.exs
+++ ex/test/chapter1_test.exs
@@ -5,4 +5,8 @@ defmodule Chapter1Test do
 	test "ex_3" do
 		assert Chapter1.Ex3.run(1,2,3) == 5
 	end
+
+	test "ex_8" do
+		assert Chapter1.Ex8.run(9) == 3.0
+	end
 end
